441c50b21ddf25c2c9f3e51b5bb25db8284f0949
Anthony Ngonde <anto.ngonde@gmail.com>
2024-06-11 21:12:26 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Container from 'react-bootstrap/Container';
+import { Button } from 'react-bootstrap';
+
+function App() {
+
+  return (
+  
+    <Container>
+    <Button>A button</Button>
+    </Container>
+   
+ 
+  )
+}
+
+export default App
